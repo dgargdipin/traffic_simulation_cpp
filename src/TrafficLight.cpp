55c3e2cab7df0d5dff16b193d84d008db8d7d5ef
Dipin Garg <dgargdipin@gmail.com>
2021-10-29 15:21:05 +0530

    final

--- src/TrafficLight.cpp
+++ src/TrafficLight.cpp
@@ -2,42 +2,6 @@
 #include <iostream>
 #include <random>
 
-/* Implementation of class "MessageQueue" */
-
-/*
-template <typename T>
-T MessageQueue<T>::receive()
-{
-    // FP.5a : The method receive should use std::unique_lock<std::mutex> and
-_condition.wait()
-    // to wait for and receive new messages and pull them from the queue using
-move semantics.
-    // The received object should then be returned by the receive function.
-}
-
-template <typename T>
-void MessageQueue<T>::send(T &&msg)
-{
-    // FP.4a : The method send should use the mechanisms
-std::lock_guard<std::mutex>
-    // as well as _condition.notify_one() to add a new message to the queue and
-afterwards send a notification.
-}
-*/
-
-/* Implementation of class "TrafficLight" */
-
-/*
-
-
-
-
-
-// virtual function which is executed in a thread
-
-
-*/
-
 TrafficLightPhase TrafficLight::getCurrentPhase() { return _currentPhase; }
 
 void TrafficLight::waitForGreen() {
